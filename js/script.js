6234f8e94ddec58f2e374c4c73847326604cb07e
Yago <peixinhoyago@gmail.com>
2022-07-22 19:08:16 -0300

    :tada: first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+class Produto {
+  constructor() {
+    this.id = 0;
+    this.nomeProduto = "";
+    this.valor = 0;
+  }
+
+  adicionar() {}
+
+  deletar() {}
+}
+
+const produto = new Produto();
